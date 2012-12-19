3708e244514e09264e5869945eba7421cf2758ca
Bilal Shabbir <bilal@bilal-HP-Notebook>
2012-12-19 21:44:41 +0500

    first commit of fgt app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FgtApp::Application.config.secret_token = '521b6b1fcd4f5c3b177f355f1aee1cb1e16cbce00e0aafdbae6a557e6c3fd1d39d5d517fb13c18b73a806f98f574a39e2cf1ddec11c64ff0d18a19a69a39b54d'
