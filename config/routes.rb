ae36b791b5d49de85bab4dfd8ab30e8d0d503c01
bilal <bilal_shhabbir111@hotmail.com>
2012-12-22 14:31:04 +0500

    added more columns to table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ FgtApp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match '/posts' => 'user#get_posts_fb_graph', :via =>:get
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
