b9bd7024e12ee3a18e5c8af9578e9b6e159b2e0b
bilal <bilal_shhabbir111@hotmail.com>
2013-01-15 02:21:10 +0500

    by bilal

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ FgtApp::Application.routes.draw do
   match '/get_all_posts' => 'user#get_all_posts', :via =>:get
   match '/get_my_posts' => 'user#get_my_posts', :via =>:get
   match '/fb_subscription' => 'user#fb_subscription', :via =>[:get, :post]
-
+  match '/post_detail' => 'user#get_post_detail', :via =>:get
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
